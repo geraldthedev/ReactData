a59dbd63ad0707871830c8906833009563c6bcc2
Gerald Bryant <hakeem1106@gmail.com>
2019-02-03 22:01:30 +0000

    setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# ReactData
---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const rap = React.createElement;
+
+class InputBase extends React.Component{
+    return rap(
+        <input>
+        </input>
+    );
+}
