7781b283b0b85d55c33af094a15971c1291f31e3
Gerald Bryant <hakeem1106@gmail.com>
2019-02-05 14:43:24 +0000

    Database works

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
-
-
 class InputBar extends React.Component{
 render() {
     return(
+        <div ClassName="inputField">
         <input>
         </input>
+        <button>Submit</button>
+        </div>
     );
   }
 
