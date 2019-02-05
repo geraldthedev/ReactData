d2e63c09a608b94bad531f48ac8f841c88013711
Gerald Bryant <hakeem1106@gmail.com>
2019-02-05 19:56:00 +0000

    connect

--- src/components/inputMod.js
+++ src/components/inputMod.js
@@ -1,4 +1,5 @@
 import React from 'react';
+const mongoose = require('mongoose');
 
 class InputBar extends React.Component{
 render() {
--- src/dbconnect.js
+++ src/dbconnect.js
@@ -1,15 +1,16 @@
-import InputBar from 'index';
-
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://gbryant:Blessedautumn1106@cluster0-shard-00-00-xutke.gcp.mongodb.net:27017,cluster0-shard-00-01-xutke.gcp.mongodb.net:27017,cluster0-shard-00-02-xutke.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
+mongoose.connect('mongodb://gbryant:Blessedautumn1106@cluster0-shard-00-00-xutke.gcp.mongodb.net:27017,cluster0-shard-00-01-xutke.gcp.mongodb.net:27017,cluster0-shard-00-02-xutke.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
+console.log("database connected");
   var userDetail = new mongoose.Schema({
       name: String,
       email: String,
       phone: Number
   });
 });
+
+module.exports = {mongoose};
